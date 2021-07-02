16efd1a524842cdedfd852862f75c0ad1b7bf53c
mooon3356 <mooon3356@gmail.com>
2021-07-02 17:15:51 +0900

    solved: 2021-07-02

--- Programmers/LEVEL_1/K번째수.js
+++ Programmers/LEVEL_1/K번째수.js
@@ -15,9 +15,7 @@ function solution(array, commands) {
     return answer;
 }
 
-// 1. commands의 길이만큼 반복문을 돌린다.
-
-// 반복문의 로직은 다음과 같다.
+// 1. commands의 길이만큼 반복문을 돌린다. (반복문의 로직은 아래과 같다.)
 // 1-1. array를 commands의 첫 번째 값과 두 번째 값을 이용하여 slice한다.
 // 1-2. slice한 배열을 오름차순으로 정렬한다.
 // 1-3. 정렬된 배열에서 commands의 세 번째 값을 이용하여 값을 가져온다.
